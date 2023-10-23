680b3990916df895c69b6b2928a646f048873830
Harit Kumar <haritkumar@Harits-MacBook-Air.local>
2023-10-23 14:54:10 +0530

    Spring Data JPA First

---
+++ src/main/java/com/springPilot/SpringPilotApplication.java
@@ -0,0 +1,13 @@
+package com.springPilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPilotApplication.class, args);
+	}
+
+}
